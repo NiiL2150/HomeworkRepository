95a924316f5d1fb17b15db6d7ac989aa95388063
NiiL2150 <nail.shamsudinov@gmail.com>
2021-05-30 17:39:50 +0400

    Snippet Creator in Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,24 +16,57 @@ namespace ConsoleApp1
             XmlTextWriter xmlWriter = null;
             try
             {
-                xmlWriter = new XmlTextWriter("Some.xml", Encoding.Unicode);
+                Console.Write("fileName: ");
+                string fileName = Console.ReadLine();
+                Console.Write("title: ");
+                string title = Console.ReadLine();
+                Console.Write("author: ");
+                string author = Console.ReadLine();
+                Console.Write("description: ");
+                string description = Console.ReadLine();
+                Console.Write("shortcut: ");
+                string shortcut = Console.ReadLine();
+                Console.Write("command: ");
+                string command = Console.ReadLine();
+
+                xmlWriter = new XmlTextWriter($"{fileName}.snippet", Encoding.UTF8);
                 xmlWriter.Formatting = Formatting.Indented;
                 xmlWriter.WriteStartDocument();
 
-                xmlWriter.WriteStartElement("users");
-                
-                xmlWriter.WriteStartElement("user");
-                xmlWriter.WriteAttributeString("Type", "Human");
-                xmlWriter.WriteStartElement("name");
-                xmlWriter.WriteString("Dmitry");
+                xmlWriter.WriteStartElement("CodeSnippets");
+                xmlWriter.WriteAttributeString("xmlns", "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet");
+
+                xmlWriter.WriteStartElement("CodeSnippet");
+                xmlWriter.WriteAttributeString("Format", "1.0.0");
+
+                xmlWriter.WriteStartElement("Header");
+
+                xmlWriter.WriteStartElement("Title");
+                xmlWriter.WriteString(title);
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("Author");
+                xmlWriter.WriteString(author);
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("Description");
+                xmlWriter.WriteString(description);
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("Shortcut");
+                xmlWriter.WriteString(shortcut);
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("Snippet");
+                xmlWriter.WriteStartElement("Code");
+
+                xmlWriter.WriteAttributeString("Language", "CSharp");
+                xmlWriter.WriteString($"<![CDATA[{command}]]>");
 
                 xmlWriter.WriteEndElement();
                 xmlWriter.WriteEndElement();
-
-                xmlWriter.WriteStartElement("user");
-                xmlWriter.WriteAttributeString("Type", "Alien");
-                xmlWriter.WriteStartElement("name");
-                xmlWriter.WriteString("Nail");
 
                 xmlWriter.WriteEndDocument();
                 Console.WriteLine("XML Created");
@@ -44,7 +77,7 @@ namespace ConsoleApp1
             }
             finally
             {
-
+                xmlWriter.Close();
             }
         }
     }
