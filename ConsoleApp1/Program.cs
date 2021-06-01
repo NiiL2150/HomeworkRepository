8629f84c29b70e13b1f7461e3631e7a983d982ba
NiiL2150 <nail.shamsudinov@gmail.com>
2021-06-01 21:44:12 +0400

    Small changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,30 +9,47 @@ using System.Xml;
 
 namespace ConsoleApp1
 {
-    class User
+    class Calc
     {
-        public User(string name, int age)
+        static public int Sum(int n1, int n2)
         {
-            Name = name;
-            Age = age;
+            return n1 + n2;
         }
 
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public void Print()
+        static public int Mult(int n1, int n2)
         {
-            Console.WriteLine($"Name: {Name}; Age: {Age};");
+            return n1 * n2;
         }
     }
+
     class Program
     {
+        public static dynamic DCast(dynamic source, Type type)
+        {
+            return Convert.ChangeType(source, type);
+        }
+
         static void Main(string[] args)
         {
-            User user = new User("Admin", 56);
-            Console.WriteLine(typeof(User));
-            Console.WriteLine(user.GetType());
-            Console.WriteLine(Type.GetType("ConsoleApp1.User", false, true));
+            Calc math = new Calc();
+            string read = Console.ReadLine();
+            foreach (System.Reflection.MethodInfo item in math.GetType().GetMethods())
+            {
+                if(String.Compare(item.Name.ToLower(), 0, read.ToLower(), 0, Math.Min(item.Name.Length, read.Length)) == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine(item.Name);
+                    Console.WriteLine(item.GetParameters().Length);
+                    object[] vs = new object[item.GetParameters().Length];
+                    for (int i = 0; i<vs.Length; i++)
+                    {
+                        Console.Write($"{item.GetParameters()[i].Name} - {item.GetParameters()[i].ParameterType.Name} - ");
+                        vs[i] = (DCast(Console.ReadLine(), item.GetParameters()[i].ParameterType));
+                    }
+                    Console.WriteLine(item.Invoke(math, vs));
+                    break;
+                }
+            }
         }
     }
 }
